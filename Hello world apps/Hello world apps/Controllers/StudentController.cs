f8416b22399260561fb0e81cfa52dec4642703bd
Yuze Li <rex6515@gmail.com>
2018-08-21 21:24:33 +0800

    create a student controller

---
+++ Hello world apps/Hello world apps/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello_world_apps.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
