111729c4a58eb12b265dd0387864bb9c15be5173
Yuze Li <rex6515@gmail.com>
2018-08-21 21:15:23 +0800

    hello world apps

---
+++ Hello world apps/Hello world apps/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello_world_apps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Student()
+        {
+            ViewBag.Message = "Your student page.";
+
+            return View();
+        }
+    }
+}
